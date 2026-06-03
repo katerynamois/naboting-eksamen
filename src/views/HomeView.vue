05c018ac77bf765b0ca2e541675e653c5200bb08
katerynamois <kateryna88@hotmail.com>
2026-06-03 16:50:37 +0200

    fix: redirect logged-in users from home to profile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script>
 import Home from '@/components/Home.vue'
+import { getSession } from '@/utils/session.js'
+
 export default {
   components: { Home },
+  mounted() {
+    if (getSession()) {
+      this.$router.replace('/profil')
+    }
+  },
   methods: {
     handleGoToProfile() {
       this.$router.push('/opret-profil')
